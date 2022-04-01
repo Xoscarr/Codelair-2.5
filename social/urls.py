f11c2fa5e318248e37f9bc75d15565a4cb9c5577
Gary Galvin <grgalvin0104@gmail.com>
2022-03-31 18:54:28 -0700

    post

---
+++ social/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import PostListView
+
+urlpatters = [
+    path('', PostListView.as_view(), name='post-list'),
+]
--- social/views.py
+++ social/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Post
 
+class PostListView(View):
+    def get(self, request, *args, **kwargs):
+        posts = Post.objects.all().order_by('-create_on')
+
+        context = {
+            'post_list': posts,     
+        }
+
+        return render(request, 'social/post_list.html', context)
 # Create your views here.
